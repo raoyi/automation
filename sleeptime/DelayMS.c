63ac8d145ab4912eb16fc5678f1b5b725e126358
RaoYi <raoyi@users.noreply.github.com>
2020-06-01 23:44:38 +0800

    Update DelayMS.c

--- sleeptime/DelayMS.c
+++ sleeptime/DelayMS.c
@@ -1,26 +1,3 @@
-/*
-#include <stdio.h>
-#include <windows.h>
-int main(int argc, char** argv)
-{
-    int t, arg;
-    if (argc == 1)
-    {
-        printf("This tool pause CLI for some seconds.\nUsage:\nToolName.exe [SecondsNumber]\nPress Ctrl+C or Enter to exit.");
-        getchar();
-        exit(0);
-    }
-    scanf(argv[1], "%d", &t);
-    while (t > 0)
-    {
-        printf("waiting %d s    \r", t);
-        Sleep(1000);  //pause 1 second
-        t--;
-    }
-    printf("\n");
-    return 0;
-}
-*/
 #include<stdio.h>
 #include<windows.h>
 int main(int argc, char* argv[])
@@ -33,7 +10,7 @@ int main(int argc, char* argv[])
     }
     int i = atoi(argv[1]);
     /*for (i = 0; i < argc; i++)
-        printf("第 %d 个参数是 %s\n", i + 1, argv[i]);*/
+        printf("碌脷 %d 赂枚虏脦脢媒脢脟 %s\n", i + 1, argv[i]);*/
     Sleep(i);
     return 0;
 }
